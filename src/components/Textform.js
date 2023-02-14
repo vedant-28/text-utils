88de6597f68555a5da1d8eeda417b09ad1544ca6
Vedant <vedant10vaidya@gmail.com>
2023-02-14 20:17:59 +0530

    Text utils video 8

--- src/components/Textform.js
+++ src/components/Textform.js
@@ -1,23 +1,43 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 export default function Textform(props) {
-    const [text, setText] = useState();
-    const handleOnChange = (e) => {
-        console.log("Change detected!");
-        setText(e.target.value);
-    }
-    const handleUppercaseClick = () => {
-        console.log("Clicked on Uppercase!");
-        let newText = text.toUpperCase();
-        setText(newText);
-    }
+  const [text, setText] = useState("");
+  const handleOnChange = (e) => {
+    setText(e.target.value);
+  };
+  const handleUppercaseClick = () => {
+    let upperCaseText = text.toUpperCase();
+    setText(upperCaseText);
+  };
+  const handleLowercaseClick = () => {
+    let lowerCaseText = text.toLowerCase();
+    setText(lowerCaseText);
+  }
   return (
-    <div>
-      <h2>{props.heading}</h2>
-      <div className="mb-3 my-3">
-        <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+    <>
+      <div className="container">
+        <h2>{props.heading}</h2>
+        <div className="mb-3 my-3">
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            id="myBox"
+            rows="8"
+          ></textarea>
+        </div>
+        <button className="btn btn-primary" onClick={handleUppercaseClick}>
+          Convert to Uppercase
+        </button>
+        <button className="btn btn-primary mx-3" onClick={handleLowercaseClick}>
+          Convert to Lowercase
+        </button>
       </div>
-      <button className="btn btn-primary" onClick={handleUppercaseClick}>Convert to Uppercase</button>
-    </div>
+      <div className="container my-3">
+        <h2>Text summery</h2>
+        <p>{text.split(" ").length} words, {text.length} characters</p>
+        <p>You need {0.008 * text.split(" ").length} minutes to read this text</p>
+      </div>
+    </>
   );
 }
